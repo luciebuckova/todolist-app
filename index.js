f5d8e7662a59966da0b2318c876f617ac595f0e4
Lucie Bučková <lucie.buckova@gmail.com>
2023-11-01 19:32:58 +0100

    Adding basic functionality without styling

--- index.js
+++ index.js
@@ -6,17 +6,34 @@ const app = express();
 const port = 3000;
 let tasks = [];
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index.ejs', { tasks: tasks });
 });
 
 app.post('/submit', (req, res) => {
   tasks.push(req.body.addTask);
-  res.render('index.ejs', { tasks: tasks });
-  console.log(tasks);
+  res.redirect('/');
+});
+
+app.post('/edit', (req, res) => {
+  res.render('edit.ejs', { tasks: tasks, task: req.body.edit });
+});
+
+app.post('/update', (req, res) => {
+  const editedTask = req.body.editedTask;
+  const taskId = req.body.taskId;
+  tasks[taskId] = editedTask;
+  res.redirect('/');
+});
+
+app.post('/delete', (req, res) => {
+  const taskId = req.body.taskId;
+  tasks.splice(taskId, 1);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
